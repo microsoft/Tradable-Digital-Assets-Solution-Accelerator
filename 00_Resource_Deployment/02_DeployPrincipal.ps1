290cb09a1e84b77f027c5679fe783bbb2b493d2a
Eric Gonzalez Fuentes <ericgf@microsoft.com>
2020-05-11 21:00:14 -0500

    new scripts

--- 00_Resource_Deployment/02_DeployPrincipal.ps1
+++ 00_Resource_Deployment/02_DeployPrincipal.ps1
@@ -1,9 +1,20 @@
-$resourceGroupName = 'CryptoGoods'
-$subscriptionID = '<subscription ID>'
-$keyvaultname = 'contosovault'
+$resourceGroupName = 'deployazure4bcatry'
+$location = 'CentralUS'
+$subscriptionID = 'dbd9230d-b929-45a9-99c3-f2c003d9c1b8'
+$kuberneteservice = 'digitalgoodscatry'
+$keyvaultname = 'contosodigitalvaultcatry'
+$keyname = 'GiftEncKey'
+$secretvalue = 'np-3ADurkOSU0hn=@.MtGhnhQEVqqw17'
+
 
 az login
 az account set --subscription $subscriptionID
+
+# Create a Kubernetes Service
+az aks create --resource-group $resourceGroupName --name $kuberneteservice --node-count 1 --enable-addons monitoring --generate-ssh-keys
+
+# Create a Key Vault   
+az keyvault create --name $keyvaultname --resource-group $resourceGroupName --location $location --subscription $subscriptionID
 
  # Create a Service Principal
 $credentials = $(az ad sp create-for-rbac --role "Contributor" --scopes "/subscriptions/$subscriptionID/resourceGroups/$resourceGroupName" -o json)
@@ -14,14 +25,19 @@ $appId = $jsonCredentials.appId
 $password = $jsonCredentials.password
 $tenant = $jsonCredentials.tenant
 
+
+
 # Update Key Vault Policy to accept Service Principal request
 az keyvault set-policy --name $keyvaultname --spn $appId `
     --certificate-permissions backup create delete deleteissuers get getissuers import list listissuers managecontacts manageissuers purge recover restore setissuers update `
     --key-permission backup create decrypt delete encrypt get import list purge recover restore sign unwrapKey update verify wrapKey `
     --secret-permissions backup delete get list purge recover restore set
 
+    az keyvault key create --name $keyname --vault-name $keyvaultname
+
+az keyvault secret set --name $keyname --vault-name $keyvaultname --value $secretvalue
  # Check access to Service Principal
-az login --service-principal -u $appId --password $password --tenant $tenant
+#az login --service-principal -u $appId --password $password --tenant $tenant
 
 $credentials | Out-File -FilePath .\Credentials.txt
 
