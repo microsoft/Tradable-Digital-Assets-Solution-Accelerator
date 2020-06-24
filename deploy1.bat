fb514d8966ac497748cd467d4eb3075aad559d60
Eric Gonzalez Fuentes <ericgf@microsoft.com>
2020-06-24 10:43:18 -0500

    Add images

--- deploy1.bat
+++ deploy1.bat
@@ -1,4 +1,4 @@
 Powershell.exe -executionpolicy remotesigned -File .\00_Resource_Deployment\DeployResources1.ps1 ^
-        --resourceGroupName contosogoods ^
+        --resourceGroupName contosogoodseric ^
         --location westus2 ^
-        --subscriptionID 50169d6e-c860-4e3a-8b15-c0bd9b190783
+        --subscriptionID dbd9230d-b929-45a9-99c3-f2c003d9c1b8
