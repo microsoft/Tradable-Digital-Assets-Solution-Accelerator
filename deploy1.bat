294c5dce1fd72f13e0bab5b8a68277e1d11f3602
DB Lee <donlee@microsoft.com>
2020-06-30 16:44:01 +0900

    Remove parameter information for subscription id

--- deploy1.bat
+++ deploy1.bat
@@ -1,4 +1,4 @@
 Powershell.exe -executionpolicy remotesigned -File .\00_Resource_Deployment\DeployResources1.ps1 ^
-        --resourceGroupName contosogoodseric ^
+        --resourceGroupName contosogoods ^
         --location westus2 ^
-        --subscriptionID dbd9230d-b929-45a9-99c3-f2c003d9c1b8
+        --subscriptionID <<Put your SubscriptionID>>
